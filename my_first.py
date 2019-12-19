9000af72090ee75e8a306f42c6c47db9271b6071
tyhclimber <yahuat@163.com>
2019-12-19 16:55:02 +0800

    some spiders updated

--- my_first.py
+++ my_first.py
@@ -288,15 +288,114 @@ if __name__ == '__main__':
 # from openpyxl import Workbook
 # from random import randint
 
+"""
+
+# chromedriver_path = "D:\EdgeDownload\chromedriver_win32\chromedriver.exe"
+# weibo_username = "oentems@sina.com"
+# weibo_password = "paigu0719."
+
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
+# 定义一个taobao类
+class taobao_infos:
 
-def num_auto(n_c):
-    return n_c * n_c
+    # 对象初始化
+    def __init__(self):
+        url = 'https://login.taobao.com/member/login.jhtml'
+        self.url = url
+
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})  # 不加载图片,加快访问速度
+        options.add_experimental_option('excludeSwitches',
+                                        ['enable-automation'])  # 此步骤很重要，设置为开发者模式，防止被各大网站识别出来使用了Selenium
+
+        self.browser = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+        self.wait = WebDriverWait(self.browser, 10)  # 超时时长为10s
+
+    # 登录淘宝
+    def login(self):
+        # 打开网页
+        self.browser.get(self.url)
+
+        # 等待 密码登录选项 出现s
+        password_login = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '.qrcode-login > .login-links > .forget-pwd')))
+        password_login.click()
+
+        # 等待 微博登录选项 出现
+        weibo_login = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.weibo-login')))
+        weibo_login.click()
+
+        # 等待 微博账号 出现
+        weibo_user = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.username > .W_input')))
+        weibo_user.send_keys(weibo_username)
+
+        # 等待 微博密码 出现
+        weibo_pwd = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.password > .W_input')))
+        weibo_pwd.send_keys(weibo_password)
+
+        # 等待 登录按钮 出现
+        submit = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.btn_tip > a > span')))
+        submit.click()
+
+        # 直到获取到淘宝会员昵称才能确定是登录成功
+        taobao_name = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                                      '.site-nav-bd > ul.site-nav-bd-l > li#J_SiteNavLogin > div.site-nav-menu-hd > div.site-nav-user > a.site-nav-login-info-nick ')))
+        # 输出淘宝昵称
+        print(taobao_name.text)
 
 
-n = 1
-while n > 0:
-    input1 = int(input("please input:"))
-    print(num_auto(input1))
-    
+# 使用教程：
+# 1.下载chrome浏览器:https://www.google.com/chrome/
+# 2.查看chrome浏览器的版本号，下载对应版本号的chromedriver驱动:http://chromedriver.storage.googleapis.com/index.html
+# 3.填写chromedriver的绝对路径
+# 4.执行命令pip install selenium
+# 5.打开https://account.weibo.com/set/bindsns/bindtaobao并通过微博绑定淘宝账号密码
+
+if __name__ == "__main__":
+    chromedriver_path = "D:\EdgeDownload\chromedriver_win32\chromedriver.exe"  # 改成你的chromedriver的完整路径地址
+    weibo_username = "onetems@sina.com"  # 改成你的微博账号
+    weibo_password = "paigu0719."  # 改成你的微博密码
+
+    a = taobao_infos()
+    a.login()  # 登录
+"""
+
+
+def gcd(x, y):
+    if x > y:
+        x, y = y, x
+    else:
+        x, y = x, y
+    for result in range(x, 0, -1):
+        if x % result == 0 and y % result == 0:
+            return result
+
+
+def gcd_real(x, y):
+    (x, y) = (y, x) if x > y else (x, y)
+    for factor in range(x, 0, -1):
+        if x % factor == 0 and y % factor == 0:
+            return factor
+
+
+def __max(x, y):
+    if x > y:
+        x, y = y, x
+    else:
+        x, y = x, y
+    for result in range(y, x * y + 1, 1):
+        if result % x == 0 and result % y == 0:
+            return result
+
+
+num1 = int(input("please input the  first number:"))
+num2 = int(input("please input the second number:"))
+print(gcd(num1, num2), __max(num1, num2))
+print(gcd_real(num1, num2))
